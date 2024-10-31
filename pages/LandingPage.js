f9fc9ed93338434f55f4dda369fc635148257714
Miyuri15 <miyurilokuhewage15@gmail.com>
2024-11-01 00:54:49 +0530

    footer done

---
+++ pages/LandingPage.js
@@ -0,0 +1,13 @@
+import Footer from '@/components/Footer'
+import React from 'react'
+
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Hello from landing page</h1>
+      <Footer/>
+    </div>
+  )
+}
+
+export default LandingPage
